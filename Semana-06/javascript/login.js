ee0b0fd198985dfdaa2b70fb082669d500e0ed16
Romina Mondello <rominamonde@gmail.com>
2022-09-25 14:06:52 -0300

    BaSP-W06-2022 login logic iniciated

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -2,26 +2,67 @@
 para el funcionamiento del formulario login. La validación de cada campo se debe realizar en el evento
 “blur” de cada uno de los campos. Además, si algún campo tiene un error de validación, en el evento “focus”
 de dicho campo debe desaparecer el mensaje porque se asume que el usuario está corrigiendo el error. */
+
+
+
 window.onload = function() {
 
 var email = document.getElementById("email");
 var password = document.getElementById("password");
 var login = document.getElementById("login");
 
+function validFormatEmail(validEmailFormat) {
+    
+    if (validEmailFormat== "rominamonde@gmail.com") {
+        return true;
+       }   
+    else {
+        return false;
+        }
+}
 
-var main = document.getElementsByClassName("login-form-section")[0];
-var validations = document.createElement("p");
-p.innerHTML = "email invalid";
-form.appendChild("p");
-validations.classList.add("validations");
+function validFormatPassword(password) {
+    
+    if (password=="mypass234") {
+        return true;
+       }   
+    else {
+        return false;
+        }
+}
+
+function validLogin (typedEmail, typedPassword) {
+    if (typedEmail=="rominamonde@gmail.com" && typedPassword=="mypass234") {
+        return true;
+    } else {
+    return false;
+    }
+}
 
 login.onclick = function(e) {
     e.preventDefault();
-    console.log(email.value);
+    
 }
 
 email.onblur = function () {
-    console.log(validations);
+    console.log(validFormatEmail(email.value));
 }
 
+password.onblur = function () {
+    console.log(validFormatPassword(password.value));
+}
+
+login.onsubmit = function () {
+    console.log(validLogin(email.value, password.value));
+}
+
+
+var myformlogin = document.getElementById("login-form");
+console.log(myformlogin);
+var myP = document.createElement("p");
+myP.innerHTML = "email invalid";
+login-form.appendChild(myP);
+
+
+//myP.classList.add("validations");
 }
