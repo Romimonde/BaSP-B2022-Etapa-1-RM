8c24202063ecc2b1fe097d6d40984e4e77f41e8d
Romina Mondello <rominamonde@gmail.com>
2022-09-25 02:21:24 -0300

    BaSP-W06-2022 login form function started

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,2 +1,27 @@
+/*Se debe crear un archivo llamado login.js en donde se debe poner toda la lógica Javascript necesaria
+para el funcionamiento del formulario login. La validación de cada campo se debe realizar en el evento
+“blur” de cada uno de los campos. Además, si algún campo tiene un error de validación, en el evento “focus”
+de dicho campo debe desaparecer el mensaje porque se asume que el usuario está corrigiendo el error. */
+window.onload = function() {
+
 var email = document.getElementById("email");
-console.log(email);
+var password = document.getElementById("password");
+var login = document.getElementById("login");
+
+
+var main = document.getElementsByClassName("login-form-section")[0];
+var validations = document.createElement("p");
+p.innerHTML = "email invalid";
+form.appendChild("p");
+validations.classList.add("validations");
+
+login.onclick = function(e) {
+    e.preventDefault();
+    console.log(email.value);
+}
+
+email.onblur = function () {
+    console.log(validations);
+}
+
+}
